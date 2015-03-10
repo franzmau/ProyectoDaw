052ef5dc04b8bb17ea536c9aaa96767c28b2f2fe
franzmau <franzmau@hotmail.com>
2015-03-10 10:56:52 -0600

    Esto son las preguntas sin interefaz grafica

---
+++ preguntas.php
@@ -0,0 +1,79 @@
+
+<html>
+<meta charset="utf-8" >
+<br>
+<label> Que tan seguido consideras que el profesor tiene dificultad para explicar un tema? </label>
+<br>
+<form action="prueba.php" method="post"enctype="multipart/form-data" name="login-form" >
+<select >
+<option value="0">----Selecciona una----</option> 
+  <option value="1">Siempre</option>
+  <option value="2">Casi siempre</option>
+  <option value="3">A veces</option>
+  <option value="4">Casi nunca</option>
+  <option value="5"> Nunca</option>
+</select>
+
+<br>
+<label> ¿Cuánta habilidad consideras tu que has desarrollado?</label>
+<br>
+<select >
+	<option value="0">----Selecciona una----</option>
+  <option value="1">Demasiada</option>
+  <option value="2"> Considerable</option>
+  <option value="3">Lo normal</option>
+  <option value="4">Un poco </option>
+  <option value="5"> Nada</option>
+</select>
+<br>
+<label>¿Consideras que tu profesor tiene iniciativa y compromiso contigo? </label>
+<br>
+<select>
+	<option value="0">---Selecciona una----</option>
+  <option value="1">Siempre</option>
+  <option value="2">Casi siempre</option>
+  <option value="3">A veces</option>
+  <option value="4">Casi nunca</option>
+  <option value="5"> Nunca</option>
+</select>
+<br>
+<label> ¿Al momento de tener una duda el profesor se preocupa por ella? </label>
+<br>
+<select>
+	<option value="0">----Selecciona una----</option>
+  <option value="1">Siempre</option>
+  <option value="2">Casi siempre</option>
+  <option value="3">A veces</option>
+  <option value="4">Casi nunca</option>
+  <option value="5"> Nunca</option>
+</select>
+
+<br>
+<label>¿Consideras que el profesor es consistenete con lo que dice y hace a lo largo del semestre?</label>
+<br>
+<select>
+	<option value="0">----Seleccione una----</option>
+  <option value="1">Siempre</option>
+  <option value="2">Casi siempre</option>
+  <option value="3">A veces</option>
+  <option value="4">Casi nunca</option>
+  <option value="5"> Nunca</option>
+</select>
+<br>
+<label>¿Consideras a la materia Interesante?</label>
+<br>
+<select>
+	<option value="0">----Selecciona una----</option>
+  <option value="1"> Muy interesante</option>
+  <option value="2">Interesante</option>
+  <option value="3">Neutra</option>
+  <option value="4">muy poco interesante</option>
+  <option value="5"> Nada interesante</option>
+</select>
+<br>
+
+
+<button type="button">Evaluar</button>
+</form>
+
+</html>
